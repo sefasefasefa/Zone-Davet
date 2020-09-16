eb2035be5e273af61d56c332ba8f201faba472fe
sefasefasefa <58180511+sefasefasefa@users.noreply.github.com>
2020-09-16 12:50:11 +0300

    Add files via upload

---
+++ davet/app/events/ready.js
@@ -0,0 +1,21 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+
+module.exports = async client => {
+    client.appInfo = await client.fetchApplication();
+    setInterval( async () => {
+      client.appInfo = await client.fetchApplication();
+    }, 60000);
+  
+  console.log(`BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("oline");
+  client.user.setGame(`Alpha Code #LegendaryCode`);
+  console.log(`BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+  require("../util/dashboard.js")(client);
+};
+
